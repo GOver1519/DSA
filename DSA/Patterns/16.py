9b47266585e08fed2e23aabbd10c6e5382208ad6
Aishwarya <124514705+GOver1519@users.noreply.github.com>
2026-06-23 13:30:01 +0530

    Add pattern printing code in 16.py

---
+++ DSA/Patterns/16.py
@@ -0,0 +1,10 @@
+n = 5
+for i in range(n, 0, -1):
+    for j in range(n - i):
+        print(" ", end = " ")
+    for k in range(1, 2*i):
+        if k == 1 or k == 2*i-1 or i == n:
+            print("*", end = " ")
+        else:
+            print(" ", end = " ")
+    print()
