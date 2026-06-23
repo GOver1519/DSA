1f06462f8cb9b93b100daad6677526e66f39b06e
Aishwarya <124514705+GOver1519@users.noreply.github.com>
2026-06-23 13:54:52 +0530

    Create 20.py for pattern printing
    Add a new pattern printing script in Python.

---
+++ DSA/Patterns/20.py
@@ -0,0 +1,9 @@
+n = 5
+for i in range(n, 0, -1):
+    for j in range(n-i):
+        print(" ", end = " ")
+    for k in range(1, i+1):
+        print(k, end = " ")
+    for l in range(i-1, 0, -1):
+        print(l, end = " ")
+    print()
